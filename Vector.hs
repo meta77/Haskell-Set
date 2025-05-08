ac6d970b1d8a17ad0bf38215b5a502fd20706aff
Meta77 <horokusa@keio.jp>
2025-05-08 22:08:48 +0900

    加算とスカラーの定義

--- Vector.hs
+++ Vector.hs
@@ -2,3 +2,10 @@
 data Vec2 = Vec2 Double Double
   deriving (Show, Eq)
 
+-- ベクトル同士の加算
+addVec2 :: Vec2 -> Vec2 -> Vec2
+addVec2 (Vec2 x1 y1) (Vec2 x2 y2) = Vec2 (x1 + x2) (y1 + y2)
+
+-- スカラー倍
+scaleVec2 :: Double -> Vec2 -> Vec2
+scaleVec2 k (Vec2 x y) = Vec2 (k * x) (k * y)
