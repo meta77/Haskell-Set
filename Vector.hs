5742d5742e297b1f89bd4d60dabab38dfcaf63ce
Meta77 <horokusa@keio.jp>
2025-05-08 22:09:13 +0900

    計算の具体例

--- Vector.hs
+++ Vector.hs
@@ -9,3 +9,15 @@ addVec2 (Vec2 x1 y1) (Vec2 x2 y2) = Vec2 (x1 + x2) (y1 + y2)
 -- スカラー倍
 scaleVec2 :: Double -> Vec2 -> Vec2
 scaleVec2 k (Vec2 x y) = Vec2 (k * x) (k * y)
+
+-- ベクトルの例
+v1 = Vec2 1.0 2.0
+v2 = Vec2 3.0 4.0
+
+-- ベクトル加算
+sumVec = addVec2 v1 v2
+-- Vec2 4.0 6.0
+
+-- スカラー倍
+scaledVec = scaleVec2 2.5 v1
+-- Vec2 2.5 5.0
