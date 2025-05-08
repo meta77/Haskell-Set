b534a2d1d0d4685024f7ce276bc31e54be22ded4
Meta77 <horokusa@keio.jp>
2025-05-08 22:12:58 +0900

    インスタンスの定義

--- Vector.hs
+++ Vector.hs
@@ -27,3 +27,14 @@ class VectorSpace v where
   zeroV :: v          -- ゼロベクトル
   (^+^) :: v -> v -> v  -- ベクトル加算
   (*^)  :: Double -> v -> v -- スカラー倍
+
+instance VectorSpace Vec2 where
+  zeroV = Vec2 0 0
+  (Vec2 x1 y1) ^+^ (Vec2 x2 y2) = Vec2 (x1 + x2) (y1 + y2)
+  k *^ (Vec2 x y) = Vec2 (k*x) (k*y)
+
+v1 = Vec2 1.0 2.0
+v2 = Vec2 3.0 4.0
+
+sumVec = v1 ^+^ v2
+scaledVec = 3.0 *^ v1
