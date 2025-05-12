cae711569ad4a595b24dff6e1290bb389afa0273
Meta77 <horokusa@keio.jp>
2025-05-12 23:21:27 +0900

    単射、全射、全単射

--- map.hs
+++ map.hs
@@ -4,3 +4,21 @@ data Mapping a b = Mapping {
   domain :: [a],      -- 定義域 (A)
   codomain :: [b]     -- 値域 (B)
 }
+
+import Data.List (nub, sort)
+
+-- 単射判定
+isInjective :: (Eq b) => Mapping a b -> Bool
+isInjective (Mapping f dom _) =
+  let images = map f dom
+  in length (nub images) == length images
+
+-- 全射判定
+isSurjective :: (Eq b) => Mapping a b -> Bool
+isSurjective (Mapping f dom codom) =
+  let images = map f dom
+  in all (`elem` images) codom
+
+-- 全単射判定
+isBijective :: (Eq b) => Mapping a b -> Bool
+isBijective m = isInjective m && isSurjective m
